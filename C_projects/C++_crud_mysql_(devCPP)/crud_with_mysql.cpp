d7a32dcbd6e997683c9cadf1f122669c96324edd
Gustavo <gustavoleandrogorges@gmail.com>
2023-05-24 12:31:35 -0300

    Create funcionario working

--- C_projects/C++_crud_mysql_(devCPP)/crud_with_mysql.cpp
+++ C_projects/C++_crud_mysql_(devCPP)/crud_with_mysql.cpp
@@ -31,6 +31,41 @@ void displayResult(MYSQL_RES* result) {
     }
 }
 
+// Criando funcionario
+void createfuncionario(MYSQL* connection, string CPF, string nome, string email, string telefone, string logradouro, string funcao, string cep, string numero, string bairro){
+	
+	//Comando mysql & passando variaveis
+	string createQuery = "INSERT INTO endereco (logradouro, cep, numero, bairro) VALUES ('" + logradouro + "', '" + cep + "' , '" + numero + "', '" + bairro + "')";
+	
+	//Executando comando mysql & mostrando mensagem em caso de sucesso
+    if (executeQuery(connection, createQuery)) {
+        cout << "Record created successfully." << endl;
+    }
+    
+    //Comando mysql selecionando ID do endereço do usuario para alocar em suas informações
+    string selectQuery = "SELECT * FROM endereco ORDER BY id_endereco DESC LIMIT 1";
+    
+    if (executeQuery(connection, selectQuery)) {
+        MYSQL_RES* result = mysql_store_result(connection);
+        
+        
+        //Alocando os valores dentro de row
+		MYSQL_ROW row = mysql_fetch_row(result);
+        
+        //Salvando valor na variavel id_endereco
+		string id_endereco = row[0];
+		
+		//Comando mysql Criando funcionario
+		createQuery = "INSERT INTO funcionario (CPF, nome, email, telefone ,funcao, id_endereco) VALUES ('" + CPF + "', '" + nome + "' , '" + email + "', '" + telefone + "' , '" + funcao + "', '" + id_endereco + "')";
+		if (executeQuery(connection, createQuery)) {
+	        cout << "Record created successfully." << endl;
+	    }
+		
+        mysql_free_result(result);
+    }
+    
+}
+
 int main() {
     MYSQL* connection = mysql_init(0);
     
@@ -46,6 +81,11 @@ int main() {
     }
     
     cout << "Connected to the database." << endl << endl;
+	
+	//Funcionando createfuncionario(connection,"349.558.899-61","Duda","gustavo.gorges@faculdadecesusc.edu.br","48 32695585","rua Evaristo Guilherme Dos santos","Desenvolvedor","50000800","1234","vargem de fora");
+	
+	
+	
 	return 0;
 }
 
