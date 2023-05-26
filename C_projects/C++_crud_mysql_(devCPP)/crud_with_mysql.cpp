95ce6bec273e6990e73f04f313a81977e9b412bf
Gustavo <gustavoleandrogorges@gmail.com>
2023-05-25 22:05:36 -0300

    Mudanças realizadas na faculdade

--- C_projects/C++_crud_mysql_(devCPP)/crud_with_mysql.cpp
+++ C_projects/C++_crud_mysql_(devCPP)/crud_with_mysql.cpp
@@ -20,9 +20,18 @@ bool executeQuery(MYSQL* connection, const string& query) {
     return true;
 }
 
-// Function to display query results
+// Mostrando valores
 void displayResult(MYSQL_RES* result) {
     MYSQL_ROW row;
+    MYSQL_FIELD* field;
+
+    // Pega o nome das colunas
+    while ((field = mysql_fetch_field(result))) {
+        cout << field->name << " ";
+    }
+    cout << endl;
+
+    // Pega os dados da linha
     while ((row = mysql_fetch_row(result))) {
         for (unsigned int i = 0; i < mysql_num_fields(result); ++i) {
             cout << row[i] << " ";
@@ -34,7 +43,7 @@ void displayResult(MYSQL_RES* result) {
 // Criando funcionario
 void createfuncionario(MYSQL* connection, string CPF, string nome, string email, string telefone, string logradouro, string funcao, string cep, string numero, string bairro){
 	
-	//Comando mysql & passando variaveis
+	//Comando mysql & passando variaveis & criando endereço
 	string createQuery = "INSERT INTO endereco (logradouro, cep, numero, bairro) VALUES ('" + logradouro + "', '" + cep + "' , '" + numero + "', '" + bairro + "')";
 	
 	//Executando comando mysql & mostrando mensagem em caso de sucesso
@@ -45,26 +54,92 @@ void createfuncionario(MYSQL* connection, string CPF, string nome, string email,
     //Comando mysql selecionando ID do endereço do usuario para alocar em suas informações
     string selectQuery = "SELECT * FROM endereco ORDER BY id_endereco DESC LIMIT 1";
     
-    if (executeQuery(connection, selectQuery)) {
-        MYSQL_RES* result = mysql_store_result(connection);
-        
-        
-        //Alocando os valores dentro de row
-		MYSQL_ROW row = mysql_fetch_row(result);
-        
-        //Salvando valor na variavel id_endereco
-		string id_endereco = row[0];
+	    if (executeQuery(connection, selectQuery)) {
+	        MYSQL_RES* result = mysql_store_result(connection);
+	        
+	        //Alocando os valores dentro de row
+			MYSQL_ROW row = mysql_fetch_row(result);
+	        
+	        //Salvando valor na variavel id_endereco
+			string id_endereco = row[0];
+			
+			//Comando mysql Criando funcionario
+			createQuery = "INSERT INTO funcionario (CPF, nome, email, telefone ,funcao, id_endereco) VALUES ('" + CPF + "', '" + nome + "' , '" + email + "', '" + telefone + "' , '" + funcao + "', '" + id_endereco + "')";
+			
+			try{
+				
+				if (executeQuery(connection, createQuery)) {
+			        cout << "Record created successfully." << endl;
+			    }
+			    
+			    }catch(...){ //Por algum motivo demoniaco a funcao catch n é executada com o erro do mysql
 		
-		//Comando mysql Criando funcionario
-		createQuery = "INSERT INTO funcionario (CPF, nome, email, telefone ,funcao, id_endereco) VALUES ('" + CPF + "', '" + nome + "' , '" + email + "', '" + telefone + "' , '" + funcao + "', '" + id_endereco + "')";
-		if (executeQuery(connection, createQuery)) {
-	        cout << "Record created successfully." << endl;
+					// Mostrando erro ocorrido
+			        //std::cerr << "Erro ao criar um funcionario: " << e.what() << std::endl;
+					
+					//Comando mysql selecionando ID do endereço para deletar
+			    	string selectQuery = "SELECT * FROM endereco ORDER BY id_endereco DESC LIMIT 1";
+				    
+				    if (executeQuery(connection, selectQuery)){
+				    	MYSQL_RES* result = mysql_store_result(connection);
+				    	
+				    	//Alocando os valores dentro de row
+						MYSQL_ROW row = mysql_fetch_row(result);
+						
+						//Salvando valor na variavel id_endereco
+						string id_endereco = row[0];
+						
+						string deleteQuery = "DELETE FROM endereco WHERE id_funcionario = '" + id_endereco + "'";
+			    		
+						if (executeQuery(connection, deleteQuery)) {
+			        		cout << "Record deleted successfully." << endl;
+			    		}
+			    		
+			    		mysql_free_result(result);
+					}
+        
+				}
+			
+	        mysql_free_result(result);
 	    }
-		
-        mysql_free_result(result);
-    }
     
 }
+
+// Read
+void ler_funcionario(MYSQL* connection,string id_funcionario){
+
+string selectQuery = "SELECT * FROM funcionario WHERE id_funcionario = '" + id_funcionario + "'";
+
+    if (executeQuery(connection, selectQuery)) {
+        MYSQL_RES* result = mysql_store_result(connection);
+        displayResult(result);
+        mysql_free_result(result);
+    }
+}
+
+// Delete
+void deletar_funcionario(MYSQL* connection,string id_funcionario){
+	
+string deleteQuery = "DELETE FROM funcionario WHERE id_funcionario = '" + id_funcionario + "'";
+    if (executeQuery(connection, deleteQuery)) {
+        cout << "Record deleted successfully." << endl;
+    }
+}
+
+
+// att
+/*
+
+void atualizar_funcionario(MYSQL* connection,int id_funcionario ,string CPF, string nome, string email, string telefone, string logradouro, string funcao, string cep, string numero, string bairro){
+
+    string updateQuery = string updateQuery = "UPDATE funcionario SET nome = '" + nome + "', email = '" + email + "', telefone = '" + telefone + "', funcao = '" + funcao + "' WHERE id_funcionario = '" + id_funcionario "'"
+    
+    if (executeQuery(connection, updateQuery)) {
+        cout << "Record updated successfully." << endl;
+    }
+}
+
+*/
 
 int main() {
     MYSQL* connection = mysql_init(0);
@@ -82,12 +157,10 @@ int main() {
     
     cout << "Connected to the database." << endl << endl;
 	
-	//Funcionando createfuncionario(connection,"349.558.899-61","Duda","gustavo.gorges@faculdadecesusc.edu.br","48 32695585","rua Evaristo Guilherme Dos santos","Desenvolvedor","50000800","1234","vargem de fora");
-	
-	
+	//Funcionando 
+	createfuncionario(connection,"349.558.899-61","Gustavo","gustavo.gorges@faculdadecesusc.edu.br","48 32695585","rua Evaristo Guilherme Dos santos","Desenvolvedor","50000800","1234","vargem de fora");
+	//ler_funcionario(connection,"1");
+	//deletar_funcionario(connection,"1");
 	
 	return 0;
 }
-
-
-
